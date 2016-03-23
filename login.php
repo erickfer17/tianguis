43e28822de4da362d592a44ef8fd05da137493d9
erickfer17 <1140118@unav.edu.mx>
2016-03-23 10:02:08 -0700

    todo mi proyecto

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="head">
+		<p>Viva La Vida</p>
+	</div>
+
+	<div id="signup">
+		<form>
+			<fieldset>
+				<legend>Login</legend>
+				<label>Nombre</label><br>
+				<input type=text name="nombre"><br>
+				<label>Password</label><br>
+				<input type=password name="Contra"><br>
+				<input type=submit name="enviar">
+			</fieldset>
+
+		</form>
+	</div>
+
+
+</body>
+
+
+
+
+
+</html>
